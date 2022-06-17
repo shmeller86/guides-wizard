f73b5d5046bfcdeaa907689cf37ed9471a887431
Ïgor Rogachevich <shmeller86@gmail.com>
2022-06-17 21:01:04 +0800

    upd

--- go.sh
+++ go.sh
@@ -2,15 +2,9 @@
 
 if ! command -v curl &> /dev/null
 then
-    echo "curl could not be found"
-    exit
+    apt install curl -y > /dev/null 2>&1
 fi
-# if exists curl; then
-# 	echo ''
-# else
-#     apt install curl -y < "/dev/null"
-# fi
-
+clear
 curl -s https://raw.githubusercontent.com/shmeller86/guides-wizard/master/utils/logo.sh | bash
 
 #wget -O go.sh --progress=bar:force:noscroll https://raw.githubusercontent.com/shmeller86/guides-wizard/master/go.sh >> /dev/null && chmod +x go.sh && ./go.sh
