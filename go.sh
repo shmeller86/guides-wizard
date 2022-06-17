b88d307523faca333b334df41da9e5390a109452
Ïgor Rogachevich <shmeller86@gmail.com>
2022-06-17 20:32:43 +0800

    upd

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#! /bin/bash
 sudo -s
 if exists curl; then
 	echo ''
@@ -6,3 +6,6 @@ else
     apt install curl -y < "/dev/null"
 fi
 curl -s https://api.nodes.guru/logo.sh | bash
+
+#wget -O go.sh https://raw.githubusercontent.com/shmeller86/guides-wizard/master/go.sh && chmod +x go.sh && ./go.sh
+#curl -s https://raw.githubusercontent.com/shmeller86/guides-wizard/master/go.sh | bash
